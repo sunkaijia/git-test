d333c52f0c523d695eb9cd47172faadc014e098b
sunkaijia <kaijia528705>
2019-02-28 21:14:51 +0800

    666修改复制的git-test +111

--- idea-git-test/src/main/java/cn/itcast/FirstClass.java
+++ idea-git-test/src/main/java/cn/itcast/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
 
-        System.out.println("hello git program");
+        System.out.println("hello 111 program");
         System.out.println("hello git program");
         System.out.println("hello git program");
         System.out.println("hello git program");
