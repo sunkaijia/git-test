5e0a621f2241f7e511caaa15759f3d62f9b37a3b
sunkaijia <kaijia528705>
2019-02-28 20:16:46 +0800

    添加test工程带git

---
+++ idea-git-test/src/main/java/cn/itcast/FirstClass.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git program");
+        System.out.println("hello git program");
+        System.out.println("hello git program");
+        System.out.println("hello git program");
+        System.out.println("hello git program");
+        System.out.println("hello git program");
+    }
+}
